49bfd949209d8e1c998699fc1c572c848aa88a4a
BlottaJose <joseblotta98@gmail.com>
2025-05-28 11:58:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+// script.js
+
+// Initialize the map centered on the world
+var map = L.map('map', {
+  fullscreenControl: true
+}).setView([20, 0], 2);
+
+// Add OpenStreetMap tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+// Load project data
+fetch('projects.json')
+  .then(res => res.json())
+  .then(projectData => {
+    const projectsByCountry = {};
+    projectData.projects.forEach(p => {
+      projectsByCountry[p.country] = p;
+    });
+
+    // Load world GeoJSON
+    fetch('https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson')
+      .then(res => res.json())
+      .then(geojson => {
+        L.geoJSON(geojson, {
+          style: feature => {
+            const cname = feature.properties.name;
+            if (projectsByCountry[cname]) {
+              return {
+                color: '#3388ff',
+                weight: 2,
+                fillColor: '#ffeeaa',
+                fillOpacity: 0.5
+              };
+            }
+            return {
+              color: '#999',
+              weight: 1,
+              fillOpacity: 0
+            };
+          },
+          onEachFeature: (feature, layer) => {
+            const cname = feature.properties.name;
+            if (projectsByCountry[cname]) {
+              const proj = projectsByCountry[cname];
+              let popupHtml = `<strong>${cname}</strong><br>`;
+              if (proj.image) {
+                popupHtml += `<img src="${proj.image}" style="width:100px;"><br>`;
+              }
+              popupHtml += proj.description || '';
+              layer.bindPopup(popupHtml);
+            }
+          }
+        }).addTo(map);
+      });
+  });
